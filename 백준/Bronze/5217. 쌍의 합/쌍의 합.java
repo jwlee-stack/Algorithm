1da34b12f3fef60db1b0b8d571b2610b4926f548
이재완 <82564800+jwlee-stack@users.noreply.github.com>
2024-02-03 23:09:04 +0900

    [Bronze III] Title: 쌍의 합, Time: 76 ms, Memory: 11432 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.java
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.java
@@ -2,22 +2,19 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-// The main method must be in a class named "Main".
 class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb=new StringBuilder();
         int N=Integer.parseInt(br.readLine());
-        int[] arr=new int[N];
         Queue<Integer> a=new LinkedList<>();
         Queue<Integer> b=new LinkedList<>();
         
         for(int i=0;i<N;i++)
-            arr[i]=Integer.parseInt(br.readLine());
-        
-        for(int i=0;i<N;i++)
         {
-            System.out.print("Pairs for "+arr[i]+": ");
-            for(int t=0,j=1,k=arr[i]-1;t<arr[i]/2;t++,j++,k--)
+            int num=Integer.parseInt(br.readLine());
+            sb.append("Pairs for ").append(num).append(": ");
+            for(int t=0,j=1,k=num-1;t<num/2;t++,j++,k--)
             {
                 if(j==k) break;
                 a.add(j);
@@ -25,12 +22,12 @@ class Main {
             }
             while(!a.isEmpty())
             {
-                if(a.size()==1)
-                    System.out.print(a.remove()+" "+b.remove());
-                else
-                    System.out.print(a.remove()+" "+b.remove()+", ");
+                sb.append(a.remove()).append(" ").append(b.remove());
+                if(a.size()>0)
+                    sb.append(", ");
             }
-            System.out.println();
+            sb.append("\n");
         }
+        System.out.print(sb);
     }
 }
